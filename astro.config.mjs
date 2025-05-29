f35711d743813d2827e8030f397265d30fe54920
Britnell <tommy.britnell@gmail.com>
2025-05-29 11:10:35 +0200

    astro init + digital clock pwa

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
+
+import tailwindcss from "@tailwindcss/vite";
+
+const pwaConfig = AstroPWA({
+  injectRegister: "script",
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true,
+    type: "module",
+  },
+  workbox: {
+    globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+  },
+  manifest: {
+    name: "My Awesome App",
+    short_name: "MyApp",
+    description: "My Awesome App description",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "alien.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "alien.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+});
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    pwaConfig,
+    // other integrations...
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
