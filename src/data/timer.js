eba65908c58ebd37dd7c4f7f4667aef2b282d8f0
Britnell <britnell.h-d@partner.akdb.de>
2025-06-08 11:32:14 +0200

    hide timer hours '00'

--- src/data/timer.js
+++ src/data/timer.js
@@ -133,6 +133,11 @@ Alpine.data('timer', () => ({
   },
 
   formatTime(hours, minutes, seconds) {
+    if (hours === 0) {
+      return `${minutes.toString().padStart(2, '0')}:${seconds
+        .toString()
+        .padStart(2, '0')}`;
+    }
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
       .toString()
       .padStart(2, '0')}`;
