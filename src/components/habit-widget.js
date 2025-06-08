d60bcfd9afa55eb054c21653a750a2361fd34953
Britnell <britnell.h-d@partner.akdb.de>
2025-06-08 11:59:24 +0200

    habit align size

--- src/components/habit-widget.js
+++ src/components/habit-widget.js
@@ -8,7 +8,7 @@ class HabitWidget extends HTMLElement {
       <div x-show="!openHabit" class=" absolute bottom-2 left-2">
         <button @click="openHabit = Object.values(habits)?.[0]" class="p-3 flex space-x-1 ">
           <template x-for="habit in Object.values(habits)">
-            <span :style="{background: habit.color}" class=" size-4 rounded-full grid place-items-center">
+            <span :style="{background: habit.color}" class=" size-5 rounded-full grid place-items-center">
               <span x-text="getFulfilledDaysCount(habit.id)" class="text-xs"></span>
             </span>
           </template>
@@ -21,7 +21,7 @@ class HabitWidget extends HTMLElement {
           <template x-for="habit in Object.values(habits)">
             <button @click="openHabit = habit" class="flex gap-1 items-center px-2 py-[0.1rem]"
                 :class="openHabit?.id === habit.id ? ' bg-[#fff5]' : ' opacity-60'">
-              <span :style="{background: habit.color}" class=" size-4 rounded-full grid place-items-center">
+              <span :style="{background: habit.color}" class=" size-5 rounded-full grid place-items-center">
                 <span x-text="getFulfilledDaysCount(habit.id)" class="text-xs"></span>
               </span> 
               <span x-text="habit.name"></span>
@@ -34,11 +34,11 @@ class HabitWidget extends HTMLElement {
               <span x-text="day.date.getDate()" class=" text-gray-600"></span>
               <span x-text="day.day"></span>
               <button @click="toggleHabit(day.date)" class="p-1">
-                <span class="size-[1.2rem] rounded-full grid place-items-center " x-show="day?.habit?.value === 1"
+                <span class="size-5 rounded-full grid place-items-center " x-show="day?.habit?.value === 1"
                   :style="{background: openHabit?.color}">
                   1
                 </span>
-                <span class="size-[1.2rem] rounded-full grid place-items-center bg-gray-300 "
+                <span class="size-5 rounded-full grid place-items-center bg-gray-300 "
                   x-show="!day?.habit?.value">0</span>
               </button>
             </div>
