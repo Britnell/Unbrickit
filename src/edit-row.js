61bec9bcf17dd55ed0837011abe257456e2f3c81
Britnell <britnell.h-d@partner.akdb.de>
2025-06-08 09:45:07 +0200

    remove astro pages & comp

--- src/edit-row.js
+++ src/edit-row.js
@@ -1,4 +1,4 @@
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
 class EditRow extends HTMLElement {
   constructor() {
--- src/vue/app.ts
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia';
-import type { App } from 'vue';
-import '../global.css';
-
-const pinia = createPinia();
-
-export default (app: App) => {
-  app.use(pinia);
-};
