f207e07c31d94126f570c2b2f7a8aeba9f765e7b
Britnell <britnell.h-d@partner.akdb.de>
2025-06-28 18:09:32 +0200

    podc tweak

--- src/components/menu-podcast.js
+++ src/components/menu-podcast.js
@@ -13,6 +13,13 @@ class MenuRadio extends HTMLElement {
     </h3>
   </a>
 
+  <button @click="loadRandomEpisode()" :disabled="loading"
+    class="w-full px-4 py-2  rounded bg-[#fff8] hover:bg-[#fff5] disabled:bg-none"
+    :class="loading ? 'opacity-50 cursor-not-allowed' : ''">
+    <span x-show="!loading">🎲 Load Random Episode</span>
+    <span x-show="loading">Loading...</span>
+  </button>
+
   <div x-show="episodeAudio" class="space-y-2">
     <div class="text-sm flex gap-4 flex-wrap justify-between">
       <strong>Current Episode:</strong>
@@ -23,17 +30,17 @@ class MenuRadio extends HTMLElement {
 
     <div class="flex items-center justify-center gap-2">
       <button @click="seekBackward()" :disabled="!episodeAudio"
-        class="flex items-center justify-center whitespace-nowrap bg-white rounded-full px-3 h-10">
+        class="flex items-center justify-center whitespace-nowrap bg-[#fff6] rounded-full px-3 h-10">
         « 15s
       </button>
 
       <button @click="togglePlayback()" :disabled="!episodeAudio"
-        class="flex items-center justify-center whitespace-nowrap bg-white rounded-full w-10 h-10">
+        class="flex items-center justify-center whitespace-nowrap bg-[#fff6] rounded-full w-10 h-10">
         <span x-text="playingPodcast ? '||' : '►'"></span>
       </button>
 
       <button @click="seekForward()" :disabled="!episodeAudio"
-        class="flex items-center justify-center whitespace-nowrap bg-white rounded-full px-3 h-10">
+        class="flex items-center justify-center whitespace-nowrap bg-[#fff6] rounded-full px-3 h-10">
         30s »
       </button>
     </div>
@@ -41,12 +48,6 @@ class MenuRadio extends HTMLElement {
 
   <p x-show="error" class=" bg-red-500/40 text-white" x-text="error"></p>
 
-  <button @click="loadRandomEpisode()" :disabled="loading"
-    class="w-full px-4 py-2  rounded bg-[#fff2] hover:bg-[#fff5] disabled:bg-none"
-    :class="loading ? 'opacity-50 cursor-not-allowed' : ''">
-    <span x-show="!loading">🎲 Load Random Episode</span>
-    <span x-show="loading">Loading...</span>
-  </button>
 
 </div>
     `;
