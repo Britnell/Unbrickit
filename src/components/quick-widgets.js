69e7dc31b12d2dddb9a57014477bfd3c114d14b1
Britnell <britnell.h-d@partner.akdb.de>
2025-06-28 18:06:13 +0200

    widget order

--- src/components/quick-widgets.js
+++ src/components/quick-widgets.js
@@ -7,6 +7,17 @@ class AlarmWidget extends HTMLElement {
     this.innerHTML = `
 <div x-show="page === 'clock'" class="absolute bottom-2 right-2 flex gap-2">
 
+  <button x-show="playingRadio" @click="showMenu = true, menu='radio'"
+    class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg ">
+    <span class="text-lg">📻</span>
+  </button>
+
+
+  <button x-show="playingPodcast" @click="showMenu = true, menu='podcast'"
+    class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg ">
+    <span class="text-lg">🎙️</span>
+  </button>
+
   <button x-show="isRunning" @click="page='timer'"
     class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg">
     <span class="text-lg">⏱️</span>
@@ -17,18 +28,6 @@ class AlarmWidget extends HTMLElement {
     class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg ">
     <span class="text-lg">⏰</span>
     <!-- <span x-text="alarmTime" class="text-xs font-mono"></span> -->
-  </button>
-
-
-  <button x-show="playingRadio" @click="showMenu = true, menu='radio'"
-    class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg ">
-    <span class="text-lg">📻</span>
-  </button>
-
-
-  <button x-show="playingPodcast" @click="showMenu = true, menu='podcast'"
-    class="px-2 py-1 bg-[#fff5] rounded-full flex items-center space-x-2 shadow-lg ">
-    <span class="text-lg">🎙️</span>
   </button>
 
 </div>
