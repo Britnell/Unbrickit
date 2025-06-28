589f36600a03f192cd85cb2ffa0512eb3f341385
Britnell <britnell.h-d@partner.akdb.de>
2025-06-28 17:15:54 +0200

    simplify menu

--- src/data/app.js
+++ src/data/app.js
@@ -2,8 +2,20 @@ import Alpine from 'alpinejs';
 
 const pages = ['clock', 'timer', 'alarm'];
 
+const menuItems = [
+  { type: 'menu', key: 'habit', label: '📊 habit' },
+  { type: 'placeholder' },
+  { type: 'menu', key: 'radio', label: '📻 radio' },
+  { type: 'menu', key: 'podcast', label: '🎙️ podcast' },
+  { type: 'page', key: 'timer', label: '⏱️ timer' },
+  { type: 'page', key: 'alarm', label: '⏰ alarm' },
+  { type: 'menu', key: 'theme', label: '🎨 theme' },
+  { type: 'menu', key: 'chime', label: '🔔 chime' },
+];
+
 Alpine.data('app', () => ({
   page: pages[0],
   menu: '',
   showMenu: false,
+  menuItems,
 }));
